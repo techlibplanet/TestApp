48c198d31b31758013e960d933dcb2264a1b86f0
techlibplanet <techlibplanet@gmail.com>
2018-07-10 17:32:46 +0530

    Commit from off

--- app/src/main/java/com/example/mayank/testapp/TransferPoints.kt
+++ app/src/main/java/com/example/mayank/testapp/TransferPoints.kt
@@ -2,19 +2,11 @@ package com.example.mayank.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.Button
 
-class AddPoints : AppCompatActivity() {
-
-
+class TransferPoints : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_points)
-
-
-
+        setContentView(R.layout.activity_transfer_points)
     }
 }
--- app/src/main/java/com/example/mayank/testapp/WalletActivity.kt
+++ app/src/main/java/com/example/mayank/testapp/WalletActivity.kt
@@ -1,15 +1,17 @@
 package com.example.mayank.testapp
 
+import android.content.Intent
 import android.icu.util.Currency
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
-class WalletActivity : AppCompatActivity() {
+class WalletActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var leftToRight: Animation
     private lateinit var rightToLeft : Animation
@@ -17,6 +19,8 @@ class WalletActivity : AppCompatActivity() {
     private lateinit var buttonWithdrawalPoints : Button
     private lateinit var buttonTransferPoints : Button
     private lateinit var buttonTransactions : Button
+
+    private var CLICKABLES = intArrayOf(R.id.buttonAddPoints, R.id.buttonWithdrawalPoints, R.id.buttonTransferPoints, R.id.buttonTransactions)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +44,29 @@ class WalletActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.amountTextView).text = "$currencySymbol 135.00"
 
+        for (id in CLICKABLES){
+            findViewById<Button>(id).setOnClickListener(this)
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.buttonAddPoints ->{
+                val intent = Intent(this, AddPoints::class.java)
+                startActivity(intent)
+            }
+
+            R.id.buttonWithdrawalPoints ->{
+                val intent = Intent(this, WithdrawalPoints::class.java)
+                startActivity(intent)
+            }
+
+            R.id.buttonTransferPoints ->{
+                val intent = Intent(this, TransferPoints::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
 
